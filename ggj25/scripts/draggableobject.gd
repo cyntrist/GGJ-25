8d0c18810073697bd7da02af4f93733c93197e19
Paigro <paigro9@gmail.com>
2025-01-24 22:33:54 +0100

    Ya esta.

--- ggj25/scripts/draggableobject.gd
+++ ggj25/scripts/draggableobject.gd
@@ -1,25 +1,21 @@
 extends Node2D
 
 var is_selected = false # Para saber si esta seleccionado y moverlo.
-var can_be_draggable = false # Para saber si se puede mover.
-var offset : Vector2 # Offset 
 
-@export var init_pos : Vector2 # Posicion inicial para volver cuando se suelte.
-
-
+@export var init_pos : Vector2 # Posicion inicial del objeto para volver cuando se suelte.
 
 const text1 = preload("res://icon.svg")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Settear la posicion al inicio.
 	global_position = init_pos
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#print("Hola")
 	# Mover el objeto.
 	if is_selected:
 		global_position = get_global_mouse_position()
