fb184966faedfc2acf524a54a05e6ad219142269
cyntrist <cyntrist@ucm.es>
2025-01-26 00:11:38 +0100

    CONTEXTO HISTÓRICO

--- ggj25/scripts/scene_scripts/contexto.gd
+++ ggj25/scripts/scene_scripts/contexto.gd
@@ -8,17 +8,19 @@ var textDisplay: float = 0
 var aumentado: bool = false
 @onready var label: Label = $Label
 @onready var background: ColorRect = $ColorRect
-
+var text_ended = false
+var clicked = false
 var clicks = 0;
+var stop = false
+var transitioned = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	label.text = "Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto "
-
+	label.text = "Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto Contexto contexto"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if elapsedTime <= maxTime:
+	if elapsedTime <= maxTime && !stop:
 		if timer >= frames_per_letter and textDisplay < 1:
 			timer = 0
 			label.visible_ratio =  textDisplay
@@ -31,8 +33,18 @@ func _process(delta: float) -> void:
 
 func _input(event):
 	if event.is_action_pressed("click"):
-		$AnimationPlayer.play("fadeout")
-		print("click")
+		if transitioned:
+			print(transitioned)
+			Global.change_scene(Global.Scenes.PREPOSTCITA)
+		if !text_ended:
+			label.visible_ratio = 1
+			stop = true;
+			text_ended = true;
+		else:
+			if !clicked:
+				$AnimationPlayer.play("fadeout")
+				clicked = true
+			
 
 
 
@@ -41,3 +53,6 @@ func on_enable():
 
 func on_disable():
 	pass
+
+func _on_animation_player_animation_finished(anim_name:StringName) -> void:
+	transitioned = true
