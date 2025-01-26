23efafd3f2aa092842a673e552aa6aff913bb87a
Cynthia Tristán <cyntrist@ucm.es>
2025-01-26 04:52:21 +0100

    Paigro: te agarro el pajaro y te lo meto por donde no te cabe.
    Co-Authored-By: Pablo Iglesias Rodrigo <119442055+paigro@users.noreply.github.com>

--- ggj25/scripts/prov_bird.gd
+++ ggj25/scripts/prov_bird.gd
@@ -2,6 +2,9 @@ extends RigidBody2D
 
 signal prov_bird_touched # Para cuando tocas el pajaro se emita una senyal y la detete quien lo tenga.
 @onready  var draggable = preload("res://scenes/draggable.tscn")
+var catch_bird = preload("res://assets/img/pajaro_dardo.png")
+
+@export var new_bird: Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,10 +14,14 @@ func _ready() -> void:
 # Para cuando se toca el pajaro.
 func _on_bird_down():
 	prov_bird_touched.emit() # Emitir.
-	var new_daggable = load("res://scenes/draggable.tscn").instantiate()
-	new_daggable.name = str("Bird", 0)
-	add_sibling(new_daggable)
-	new_daggable.position = global_position
+	new_bird.global_position = global_position
+	#var new_daggable = load("res://scenes/draggable.tscn").instantiate()
+	#new_daggable.name = str("Bird", 0)
+	#add_sibling(new_daggable)
+	#new_daggable.position = global_position
+	#new_daggable.dragType = 3
+	#new_daggable.get_child(2).set_button_icon(catch_bird)
+	#new_daggable.scale = Vector2(0.3, 0.3)
 	$AnimationPlayer.stop() # Parar la animacion.
 	queue_free() # Eliminarse a si mismo.
 	pass
